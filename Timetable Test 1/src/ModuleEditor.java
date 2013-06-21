2e20b33c25d9cf8462375bd1b5de8372373ba023
garuhhh <garuhhh@gmail.com>
2013-06-21 14:49:02 +0800

    added a Module Editor viewer

---
+++ Timetable Test 1/src/ModuleEditor.java
@@ -0,0 +1,96 @@
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Button;
+
+
+public class ModuleEditor extends Dialog {
+
+	protected Object result;
+	protected Shell shell;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public ModuleEditor(Shell parent, int style) {
+		super(parent, style);
+		setText("SWT Dialog");
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shell.open();
+		shell.layout();
+		Display display = getParent().getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shell = new Shell(getParent(), getStyle());
+		shell.setSize(604, 300);
+		shell.setText(getText());
+		
+		Label lblModuleNamePlaceholder = new Label(shell, SWT.NONE);
+		lblModuleNamePlaceholder.setBounds(10, 10, 233, 15);
+		lblModuleNamePlaceholder.setText("Module Code Placeholder");	//put in the Module Code in this text
+		
+		Label lblModuleNamePlaceholder_1 = new Label(shell, SWT.NONE);
+		lblModuleNamePlaceholder_1.setText("Module Name Placeholder");	//put in the Module Name in this text
+		lblModuleNamePlaceholder_1.setBounds(10, 31, 233, 15);
+		
+		Label lblPleaseSelectThe = new Label(shell, SWT.NONE);
+		lblPleaseSelectThe.setBounds(10, 73, 531, 15);
+		lblPleaseSelectThe.setText("Please select the Programmes (and the corresponding University) to which this Module is offered:");
+		
+		Combo combo = new Combo(shell, SWT.NONE);
+		combo.setBounds(10, 94, 272, 23);
+		
+		Combo combo_1 = new Combo(shell, SWT.NONE);
+		combo_1.setBounds(288, 94, 300, 23);
+		
+		Combo combo_2 = new Combo(shell, SWT.NONE);
+		combo_2.setBounds(10, 123, 272, 23);
+		
+		Combo combo_3 = new Combo(shell, SWT.NONE);
+		combo_3.setBounds(288, 123, 300, 23);
+		
+		Combo combo_4 = new Combo(shell, SWT.NONE);
+		combo_4.setBounds(10, 152, 272, 23);
+		
+		Combo combo_5 = new Combo(shell, SWT.NONE);
+		combo_5.setBounds(288, 152, 300, 23);
+		
+		Combo combo_6 = new Combo(shell, SWT.NONE);
+		combo_6.setBounds(10, 181, 272, 23);
+		
+		Combo combo_7 = new Combo(shell, SWT.NONE);
+		combo_7.setBounds(288, 181, 300, 23);
+		
+		Button btncancel = new Button(shell, SWT.NONE);
+		btncancel.setBounds(513, 237, 75, 25);
+		btncancel.setText("&Cancel");
+		
+		Button btnsave = new Button(shell, SWT.NONE);
+		btnsave.setBounds(432, 237, 75, 25);
+		btnsave.setText("&Save");
+
+	}
+
+}
